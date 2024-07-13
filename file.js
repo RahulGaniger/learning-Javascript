6ddb393802b2acf1895ec09698182eb4f1d17008
Rahul Ganiger <rahulganiger1709@gmail.com>
2024-07-13 12:20:31 +0530

    learned datatypes

---
+++ file.js
@@ -0,0 +1,17 @@
+// console.log("rahhiol");
+
+// alert(2+2)      //null is a object
+
+
+// conversion 
+
+// let num = "23as"
+// let numString = Number(num)
+// console.log( numString);
+
+/*
+primitive: 7 types  these all are call by values 
+Number, String, boolean, null, undefined, symbol, bigInt
+symbol will give unique id
+
+*/
